8a96323bd8174e27957a70306d5530e8cc04a3b9
lime-owl <tomkferris@gmail.com>
2019-10-18 11:35:49 -0700

    update class

--- DBconnection/Program.cs
+++ DBconnection/Program.cs
@@ -1,14 +1,35 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace DBconnection
 {
     class Program
     {
+        class SqlHelper
+        {
+            public static SqlDataReader ExecuteReader(String connString, String commandText)
+            {
+                SqlConnection conn = new SqlConnection(connString);
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                    return reader;
+                }
+            }
+        }
         static void Main(string[] args)
         {
             String connString = "Data Source=.; Initial Catalog=pubs; Integrated Security=true;";
             String commandText = "SELECT au_fname, au_lname FROM authors;";
-            
+
+            using (SqlDataReader reader = SqlHelper.ExecuteReader(connString, commandText))
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("Item");
+                }
+            }
 
             Console.WriteLine("Hello World!");
             Console.WriteLine("And Hello World! Again");
