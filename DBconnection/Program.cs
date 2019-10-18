fd8404bf097cfeea943ce6622efe897d7a271d03
lime-owl <tomkferris@gmail.com>
2019-10-18 10:27:39 -0700

    added a space

--- DBconnection/Program.cs
+++ DBconnection/Program.cs
@@ -9,6 +9,7 @@ namespace DBconnection
             String connString = "Data Source=.; Initial Catalog=pubs; Integrated Security=true;";
             String commandText = "SELECT au_fname, au_lname FROM authors;";
             
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("And Hello World! Again");
             Console.Read();
