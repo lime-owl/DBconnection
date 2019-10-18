82d95aef5b8e4800bb70d1f510120285eb668b20
lime-owl <tomkferris@gmail.com>
2019-10-18 10:17:15 -0700

    -

--- DBconnection/Program.cs
+++ DBconnection/Program.cs
@@ -7,6 +7,7 @@ namespace DBconnection
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("And Hello World! Again");
             Console.Read();
         }
     }
